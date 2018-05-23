dfcd2e38c59ba56dee0400dd606f88d9ac47687c
Clovis <clovis465@hotmail.fr>
2018-05-23 14:03:19 +0200

    Add Jena + creating project

---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Clovis on 23/05/2018.
+ */
+public class Utils
+{
+	public static void print(String args){
+		System.out.println(args);
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Clovis on 23/05/2018.
+ */
+public class main
+{
+	public static void main(String [] args){
+
+	}
+
+	public static void test(){
+		Utils.print("Debut test");
+
+		Utils.print("Fin test");
+	}
+}
