bbd53f166f9d0ff815e16ec3f37c063d5d24353a
Clovis <clovis465@hotmail.fr>
2018-05-23 14:43:59 +0200

    j

--- src/main.java
+++ src/main.java
@@ -1,10 +1,16 @@
+import javax.annotation.Resource;
+import java.beans.Statement;
+import java.io.InputStream;
+
 /**
  * Created by Clovis on 23/05/2018.
  */
 public class main
 {
-	public static void main(String [] args){
-
+	public static void main(String [] args)
+	{
+		// Création du modèle
+		Model model = ModelFactory.createDefaultModel();
 	}
 
 	public static void test(){
